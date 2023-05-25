ad6fb718b563652981444007d2629352d73ae027
Muhammed-Hagras <40708938+Muhammed-Hagras@users.noreply.github.com>
2023-05-26 00:04:59 +0200

    marker and form appers on click

--- script.js
+++ script.js
@@ -10,3 +10,62 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+// https://www.google.com/maps/search/%D9%86%D8%B4%D8%A7%D8%B7%D8%A7%D8%AA+%D9%85%D9%82%D8%AA%D8%B1%D9%8E%D8%AD%D8%A9%E2%80%AD/@30.3126,31.3131,10z?entry=ttu
+
+let map, mapEvent;
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
+      map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      //on map click handler
+      map.on('click', e => {
+        mapEvent = e;
+        console.log(mapEvent);
+        form.classList.remove('hidden');
+      });
+    },
+    () => {
+      alert('Sorry! your Browser does not support Geolocation API');
+    }
+  );
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  //Clearing inputs
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+//Toggling Selecion
+
+inputType.addEventListener('change', e => {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
